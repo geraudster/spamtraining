880a15beb18ca6651c777a7cf65ba41788d566c6
geraudster <geraud.dugedebernonville@gmail.com>
2015-05-13 18:37:24 +0000

    initial import

---
+++ sandbox.R
@@ -0,0 +1,64 @@
+# install.packages('ElemStatLearn')
+# library(ElemStatLearn)
+# data(spam)
+# 
+# colnames(spam)
+# 
+# spam$spam
+# spam[58]
+# spam[[58]]
+# 
+# summary(spam)
+# sapply(spam, class)
+# 
+
+loadDataFile <- function(url, dest, unzip=TRUE) {
+  ## Check data directory 
+  if(!file.exists("data")) {
+    dir.create("data")
+  }
+  
+  ## Download file if needed
+  if(!file.exists(dest)) {
+    download.file(url,
+                  dest,
+                  method='curl')
+    
+  }
+  
+  if(unzip) {
+    ## Unzip it in data dir
+    unzip(dest, exdir="data")
+  }
+}
+
+loadDataFile('https://archive.ics.uci.edu/ml/machine-learning-databases/spambase/spambase.zip',
+             'spambase')
+
+spambase <- read.csv('data/spambase.data')
+spambase.names <- readLines('data/spambase.names')
+spambase.names <- sub(':.*$', '', spambase.names[34:length(spambase.names)])
+spambase.names [length(spambase.names) + 1] <- 'spam'
+
+colnames(spambase) <- spambase.names
+spambase$spam <- as.factor(spambase$spam)
+
+hist(spambase$word_freq_order[spambase$spam == 0], col = 'blue', breaks = 20)
+hist(spambase$word_freq_order[spambase$spam == 1], col = 'red', add = T, breaks = 20)
+
+boxplot(word_freq_order ~ spam, data = spambase, col = c('blue', 'red'))
+
+spambase.cor <- cor(spambase[-58])
+
+set.seed(123)
+spambase.train.idx <- sample.int(nrow(spambase), nrow(spambase) * 0.8)
+spambase.train <- spambase[spambase.train.idx,]
+spambase.test <- spambase[-spambase.train.idx,]
+
+model1 <- glm(spam ~ ., data = spambase.train, family = "binomial")
+
+predictions1 <- predict(model1, newdata = spambase.test, type = "response")
+predictions1 <- sapply(predictions1, function (x) { if (x>0.2) 1 else 0})
+
+mean(predictions1 == spambase.test$spam)
+
