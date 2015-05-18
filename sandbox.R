3377aad6f74ad32dc0c8535816976eec8cfbb02c
geraudster <geraud.dugedebernonville@gmail.com>
2015-05-18 20:37:45 +0000

    add meetup agenda

--- sandbox.R
+++ sandbox.R
@@ -41,10 +41,10 @@ spambase.names <- sub(':.*$', '', spambase.names[34:length(spambase.names)])
 spambase.names [length(spambase.names) + 1] <- 'spam'
 
 colnames(spambase) <- spambase.names
-spambase$spam <- as.factor(spambase$spam)
+spambase$spam <- factor(spambase$spam, c(0,1), labels = c('nospam', 'spam'))
 
-hist(spambase$word_freq_order[spambase$spam == 0], col = 'blue', breaks = 20)
-hist(spambase$word_freq_order[spambase$spam == 1], col = 'red', add = T, breaks = 20)
+hist(spambase$word_freq_order[spambase$spam == 'nospam'], col = 'blue', breaks = 20)
+hist(spambase$word_freq_order[spambase$spam == 'spam'], col = 'red', add = T, breaks = 20)
 
 boxplot(word_freq_order ~ spam, data = spambase, col = c('blue', 'red'))
 
@@ -55,11 +55,11 @@ spambase.train.idx <- sample.int(nrow(spambase), nrow(spambase) * 0.8)
 spambase.train <- spambase[spambase.train.idx,]
 spambase.test <- spambase[-spambase.train.idx,]
 
-model1 <- glm(spam ~ word_freq_order + capital_run_length_total, data = spambase.train, family = "binomial")
-
+model1 <- glm(spam ~ ., data = spambase.train, family = "binomial")
+summary(model1)
 # predictions1 <- predict(model1, newdata = spambase.test, type = 'term')
 predictions1 <- predict(model1, newdata = spambase.test, type = "response")
-predictions1 <- sapply(predictions1, function (x) { if (x>0.5) 1 else 0})
+predictions1 <- sapply(predictions1, function (x) { if (x>0.7) 'spam' else 'nospam'})
 
 table(predict = predictions1, actual = spambase.test$spam)
 
@@ -67,6 +67,21 @@ mean(predictions1 == spambase.test$spam)
 
 # install.packages('ggplot2')
 library(ggplot2)
-qplot(log(spambase.test$word_freq_order + 1), predictions1)
+qplot(log(spambase.test$word_freq_order + 1), predictions1, col = spambase.test$spam)
 plot(log(spambase.test$word_freq_order + 1), predictions1, col = spambase$spam)
 
+
+library(rpart)
+model2 <- rpart(spam ~ ., data = spambase.train)
+summary(model2)
+plot(model2)
+text(model2)
+
+predictions2 <- predict(model2, newdata = spambase.test, type = "class")
+# predictions1 <- sapply(predictions1, function (x) { if (x>0.7) 'spam' else 'nospam'})
+
+table(predict = predictions2, actual = spambase.test$spam)
+
+mean(predictions2 == spambase.test$spam)
+
+# install.packages('caret')
